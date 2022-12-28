3ac26005588aad079f62435c3617691deb2e7546
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-12-28 15:47:11 +0100

    Refactor Gradle build (#92)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kotlinx-serialization-flf/build.gradle.kts
+++ kotlinx-serialization-flf/build.gradle.kts
@@ -1,35 +1,10 @@
 plugins {
-    kotlin("multiplatform")
-    kotlin("plugin.serialization")
+    mpp
+    setup
+    publish
 }
 
 kotlin {
-    jvm()
-    js(IR) {
-        browser()
-        nodejs()
-    }
-
-    linuxX64()
-
-    macosX64()
-    macosArm64()
-
-    iosArm64()
-    iosArm32()
-    iosX64()
-    iosSimulatorArm64()
-
-    watchosArm32()
-    watchosArm64()
-    watchosSimulatorArm64()
-
-    tvosArm64()
-    tvosX64()
-    tvosSimulatorArm64()
-
-    mingwX64()
-
     sourceSets {
         val serialization = "1.4.1"
         commonMain {
