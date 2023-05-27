83528612e34cdc493a64dfb4af2316ec8f996d76
hfhbd <hfhbd@users.noreply.github.com>
2023-05-27 21:38:14 +0200

    Migrate to versions catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 import io.gitlab.arturbosch.detekt.*
 
 plugins {
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-    org.jetbrains.kotlinx.kover
-    org.jetbrains.dokka
-    io.gitlab.arturbosch.detekt
-    org.jetbrains.kotlinx.`binary-compatibility-validator`
+    id("io.github.gradle-nexus.publish-plugin")
+    id("org.jetbrains.kotlinx.kover")
+    id("org.jetbrains.dokka")
+    id("io.gitlab.arturbosch.detekt")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator")
 }
 
-tasks.dokkaHtmlMultiModule.configure {
+tasks.dokkaHtmlMultiModule {
     includes.from("README.md")
 }
 
 nexusPublishing {
-    repositories {
+    this.repositories {
         sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
@@ -22,13 +22,14 @@ nexusPublishing {
 }
 
 detekt {
-    source = files(rootProject.rootDir)
+    source.from(files(rootProject.rootDir))
     parallel = true
+    autoCorrect = true
     buildUponDefaultConfig = true
 }
 
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${detekt.toolVersion}")
 }
 
 tasks {
@@ -44,7 +45,6 @@ tasks {
     }
     withType<Detekt>().configureEach {
         config()
-        autoCorrect = true
 
         reports {
             sarif.required.set(true)
--- kotlinx-serialization-csv/build.gradle.kts
+++ kotlinx-serialization-csv/build.gradle.kts
@@ -1,21 +1,20 @@
 plugins {
-    mpp
-    setup
-    publish
+    id("mpp")
+    id("setup")
+    id("publish")
 }
 
 kotlin {
     sourceSets {
-        val serialization = "1.5.1"
         commonMain {
             dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-serialization-core:$serialization")
+                api(libs.serialization.core)
             }
         }
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation(libs.datetime)
             }
         }
     }
