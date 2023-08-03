cedd09ebced6692168886089372d248cce8fdd6d
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-08-03 09:56:37 +0200

    Add build scans (#140)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,19 @@ pluginManagement {
 
 plugins {
     id("MyRepos")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+    id("com.gradle.enterprise") version "3.14.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI") != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
