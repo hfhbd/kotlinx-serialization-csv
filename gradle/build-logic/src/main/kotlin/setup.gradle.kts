e29cec500ab44a0bf9dad3ef9211c317bd0d88c4
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-12-12 20:36:51 +0100

    Fix docs

--- gradle/build-logic/src/main/kotlin/setup.gradle.kts
+++ gradle/build-logic/src/main/kotlin/setup.gradle.kts
@@ -11,26 +11,19 @@ licensee {
 dokka {
     val module = project.name
     dokkaSourceSets.configureEach {
-        reportUndocumented.set(true)
         includes.from("README.md")
+        reportUndocumented.set(true)
         val sourceSetName = name
         File("$module/src/$sourceSetName").takeIf { it.exists() }?.let {
             sourceLink {
                 localDirectory.set(file("src/$sourceSetName/kotlin"))
-                remoteUrl.set(uri("https://github.com/hfhbd/kotlinx-uuid/tree/main/$module/src/$sourceSetName/kotlin"))
+                remoteUrl.set(uri("https://github.com/hfhbd/kotlinx-serialization-csv/tree/main/$module/src/$sourceSetName/kotlin"))
                 remoteLineSuffix.set("#L")
             }
         }
         externalDocumentationLinks {
             register("kotlinx.serialization") {
                 url("https://kotlinlang.org/api/kotlinx.serialization/")
-            }
-            register("kotlinx.datetime") {
-                url("https://kotlinlang.org/api/kotlinx-datetime/")
-                packageListUrl("https://kotlinlang.org/api/kotlinx-datetime/kotlinx-datetime/package-list")
-            }
-            register("sqldelight") {
-                url("https://cashapp.github.io/sqldelight/2.0.2/2.x/")
             }
         }
     }
