83ed16ec8a1d07c61dc9b6d63382eac50e539ba5
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-02-20 19:51:39 +0100

    CSV: Support decoding quoted and unordered values (#267)
    * Support unordered
    
    * Support quoted

--- kotlinx-serialization-csv/src/commonMain/kotlin/app/softwork/serialization/csv/CSVDecoder.kt
+++ kotlinx-serialization-csv/src/commonMain/kotlin/app/softwork/serialization/csv/CSVDecoder.kt
@@ -11,44 +11,86 @@ public interface CSVDecoder : Decoder, CompositeDecoder {
 
 @ExperimentalSerializationApi
 internal class CSVDecoderImpl(
-    private val data: List<List<String>>,
-    public override val configuration: CSVConfiguration,
+    private val header: List<String>,
+    private val nodes: StatefulIterator<CSVNode>,
+    override val configuration: CSVConfiguration,
+    val decodesSequentially: Boolean,
+    private val level: Int,
 ) : AbstractDecoder(), CSVDecoder {
+
+    private var index = 0
+    private var currentRow = 0
 
     override val serializersModule: SerializersModule get() = configuration.serializersModule
 
-    private var index = 0
-    private var level = 0
-    private var currentRow = 0
+    override fun beginStructure(descriptor: SerialDescriptor): CompositeDecoder {
+        when (descriptor.kind) {
+            StructureKind.CLASS -> {
+                val (nextHeaders, isSequentially) = if (configuration.includeHeader) {
+                    val nextHeaders = header.subList(index, header.size)
+                    nextHeaders to nextHeaders.isSequentially(descriptor)
+                } else {
+                    header to true
+                }
+                return CSVDecoderImpl(
+                    header = nextHeaders,
+                    nodes = nodes,
+                    configuration = configuration,
+                    decodesSequentially = isSequentially,
+                    level = level + 1,
+                )
+            }
 
-    override fun beginStructure(descriptor: SerialDescriptor): CSVDecoder {
-        if (descriptor.kind !is StructureKind.LIST) {
-            level += 1
+            PolymorphicKind.SEALED -> {
+                return CSVDecoderImpl(
+                    header = header,
+                    nodes = nodes,
+                    configuration = configuration,
+                    decodesSequentially = true,
+                    level = level,
+                )
+            }
+
+            else -> {
+                return CSVDecoderImpl(
+                    header = header,
+                    nodes = nodes,
+                    configuration = configuration,
+                    decodesSequentially = false,
+                    level = level,
+                )
+            }
         }
-        return this
     }
 
     override fun decodeNotNullMark(): Boolean {
-        val value = data[currentRow].getOrNull(index)
-        return value != null && value != ""
+        val isNotNull = when (val nextValue = nodes.peek()) {
+            is CSVNode.Element -> nextValue.value.isNotEmpty()
+            CSVNode.NewLine -> false
+            null -> false
+        }
+        return isNotNull
     }
 
-    override fun endStructure(descriptor: SerialDescriptor) {
-        level -= 1
-        if (level == 0) {
-            currentRow += 1
-            index = 0
+    override tailrec fun endStructure(descriptor: SerialDescriptor) {
+        val next = nodes.peek()
+        when (next) {
+            is CSVNode.Element -> {
+                if (level == 0) {
+                    nodes.next()
+                    endStructure(descriptor)
+                }
+            }
+
+            CSVNode.NewLine -> nodes.next()
+            null -> {}
         }
     }
 
-    override fun decodeElementIndex(descriptor: SerialDescriptor): Int {
-        error("Never called, because decodeSequentially returns true")
-    }
-
-    override fun decodeCollectionSize(descriptor: SerialDescriptor): Int = data.size
-    override fun decodeSequentially(): Boolean = true
-
     override fun decodeNull(): Nothing? {
+        if (nodes.hasNext()) {
+            nodes.next()
+        }
         index += 1
         return null
     }
@@ -59,7 +101,10 @@ internal class CSVDecoderImpl(
 
     override fun decodeShort(): Short = decodeString().toShort()
 
-    override fun decodeInt(): Int = decodeString().toInt()
+    override fun decodeInt(): Int {
+        val s = decodeString()
+        return s.toInt()
+    }
 
     override fun decodeLong(): Long = decodeString().toLong()
 
@@ -78,14 +123,45 @@ internal class CSVDecoderImpl(
     override fun decodeChar(): Char = decodeString().single()
 
     override fun decodeString(): String {
-        val value = data[currentRow].getOrNull(index) ?: throw SerializationException(
-            "Missing value at index $index in line ${currentRow + READABLE_LINE_NUMBER + HEADER_OFFSET}"
-        )
         index += 1
-        return value
+        return when (val nextNode = nodes.next()) {
+            is CSVNode.Element -> nextNode.value
+            CSVNode.NewLine -> throwUnknownValue()
+        }
     }
 
+    private fun throwUnknownValue(): Nothing = throw SerializationException(
+        "Missing value at the end of line ${currentRow + READABLE_LINE_NUMBER + HEADER_OFFSET}"
+    )
+
     override fun decodeEnum(enumDescriptor: SerialDescriptor): Int = enumDescriptor.elementNames.indexOf(decodeString())
+
+    override fun decodeSequentially(): Boolean {
+        val r = decodesSequentially
+        return r
+    }
+
+    override fun decodeElementIndex(descriptor: SerialDescriptor): Int {
+        if (descriptor.kind is StructureKind.LIST) {
+            val nextValue = nodes.peek()
+            return if (nextValue == null) {
+                CompositeDecoder.DECODE_DONE
+            } else {
+                currentRow++
+            }
+        } else {
+            val headerName = header.getOrNull(index) ?: return CompositeDecoder.DECODE_DONE
+            val index = descriptor.getElementIndex(headerName)
+            if (index == CompositeDecoder.UNKNOWN_NAME) {
+                throwUnknownValue()
+            }
+            return index
+        }
+    }
+
+    override fun <T : Any> decodeNullableSerializableValue(deserializer: DeserializationStrategy<T?>): T? {
+        return super<AbstractDecoder>.decodeNullableSerializableValue(deserializer)
+    }
 }
 
 private const val READABLE_LINE_NUMBER = 1
---
+++ kotlinx-serialization-csv/src/commonTest/kotlin/app/softwork/serialization/csv/StatefulIteratorTest.kt
@@ -0,0 +1,60 @@
+package app.softwork.serialization.csv
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StatefulIteratorTest {
+
+    @Test
+    fun normal() {
+        val iter = iterator {
+            yield(1)
+            yield(2)
+            yield(3)
+            yield(4)
+        }.stateful()
+
+        assertEquals(null, iter.current)
+
+        assertEquals(1, iter.peek())
+        assertEquals(1, iter.next())
+        assertEquals(1, iter.current)
+
+        assertEquals(2, iter.peek())
+        assertEquals(2, iter.peek())
+        assertEquals(2, iter.next())
+        assertEquals(2, iter.current)
+
+        assertEquals(3, iter.peek())
+        assertEquals(3, iter.next())
+        assertEquals(3, iter.current)
+
+        assertEquals(4, iter.next())
+        assertEquals(4, iter.current)
+
+        assertEquals(null, iter.peek())
+        assertEquals(false, iter.hasNext())
+        assertEquals(null, iter.current)
+    }
+
+    @Test
+    fun asList() {
+        val iter = iterator {
+            yield(1)
+            yield(2)
+            yield(3)
+        }.stateful()
+
+        assertEquals(listOf(1, 2, 3), iter.asSequence().toList())
+    }
+
+    @Test
+    fun empty() {
+        val iter = iterator<Int> {}.stateful()
+
+        assertEquals(null, iter.peek())
+        assertEquals(false, iter.hasNext())
+
+        assertEquals(listOf(), iter.asSequence().toList())
+    }
+}
