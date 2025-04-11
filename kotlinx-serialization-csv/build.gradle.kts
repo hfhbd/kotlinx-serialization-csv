8c4fabf10c36a989a70cf1dba13a29edd2d6554a
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-04-11 15:02:16 +0200

    Add JPMS support (#280)
    * Bump serialization from 1.8.0 to 1.8.1
    
    Bumps `serialization` from 1.8.0 to 1.8.1.
    
    Updates `org.jetbrains.kotlinx:kotlinx-serialization-core` from 1.8.0 to 1.8.1
    - [Release notes](https://github.com/Kotlin/kotlinx.serialization/releases)
    - [Changelog](https://github.com/Kotlin/kotlinx.serialization/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/Kotlin/kotlinx.serialization/compare/v1.8.0...v1.8.1)
    
    Updates `org.jetbrains.kotlinx:kotlinx-serialization-json` from 1.8.0 to 1.8.1
    - [Release notes](https://github.com/Kotlin/kotlinx.serialization/releases)
    - [Changelog](https://github.com/Kotlin/kotlinx.serialization/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/Kotlin/kotlinx.serialization/compare/v1.8.0...v1.8.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.kotlinx:kotlinx-serialization-core
      dependency-version: 1.8.1
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: org.jetbrains.kotlinx:kotlinx-serialization-json
      dependency-version: 1.8.1
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump Kotlin to 2.1.20
    
    * Add JPMS support
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- kotlinx-serialization-csv/build.gradle.kts
+++ kotlinx-serialization-csv/build.gradle.kts
@@ -19,3 +19,17 @@ kotlin {
         }
     }
 }
+
+tasks.compileJava9Java {
+    options.compilerArgumentProviders += object : CommandLineArgumentProvider {
+
+        @InputFiles
+        @PathSensitive(PathSensitivity.RELATIVE)
+        val kotlinClasses = tasks.compileKotlinJvm.flatMap { it.destinationDirectory }
+
+        override fun asArguments(): List<String> = listOf(
+            "--patch-module",
+            "app.softwork.serialization.csv=${kotlinClasses.get().asFile.absolutePath}"
+        )
+    }
+}
---
+++ kotlinx-serialization-flf/src/jvmJava9/java/module-info.java
@@ -0,0 +1,7 @@
+module app.softwork.serialization.flf {
+    exports app.softwork.serialization.flf;
+
+    requires kotlin.stdlib;
+
+    requires transitive kotlinx.serialization.core;
+}
