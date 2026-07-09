3bf989f34b5ef3f93664b72f82ada9abfd988609
hfhbd <22521688+hfhbd@users.noreply.github.com>
2026-07-09 10:27:32 +0200

    Move flf into separate repo

--- kotlinx-serialization-flf/src/commonMain/kotlin/app/softwork/serialization/flf/FixedLength.kt
+++
@@ -1,9 +0,0 @@
-package app.softwork.serialization.flf
-
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerialInfo
-
-@ExperimentalSerializationApi
-@Target(AnnotationTarget.PROPERTY)
-@SerialInfo
-public annotation class FixedLength(val length: Int)
